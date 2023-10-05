d0b3571588d212598e1a7a983f7f106087bbc2c7
Robin Stumm <robin.stumm@iohk.io>
2023-10-05 20:58:16 +0200

    fix nix build

--- nix/packages/cizero/default.nix
+++ nix/packages/cizero/default.nix
@@ -20,6 +20,11 @@
 
       zigBuildArgs = [ "-Doptimize=ReleaseSafe" ];
 
+      zigDepHashes = {
+        cron = "1ci56xsrkzdr2js5k3qsyb6pm2awslp481lif1c1mljg85swq351";
+        datetime = "15skn6rhybxkjr0jcj866blpr4pdxp072n2nhcfgngcv52d3r46w";
+      };
+
       preCheck = ''
         # for wasmtime cache
         export HOME="$TMPDIR"
