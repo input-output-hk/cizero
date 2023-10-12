a9224af4184ffa5221d397294a749d8d10bf1475
Robin Stumm <robin.stumm@iohk.io>
2023-10-12 15:19:24 +0200

    refer to tests

--- src/modules.zig
+++ src/modules.zig
@@ -1,3 +1,7 @@
 pub const Process = @import("modules/Process.zig");
 pub const Timeout = @import("modules/Timeout.zig");
 pub const ToUpper = @import("modules/ToUpper.zig");
+
+test {
+    _ = @import("std").testing.refAllDecls(@This());
+}
