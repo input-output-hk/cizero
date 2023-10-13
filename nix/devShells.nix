eb4a9240a648f6a7b6005b1421bcbb8cab27befb
Robin Stumm <robin.stumm@iohk.io>
2023-10-13 17:46:44 +0200

    add build-plugins command to mission-control

--- nix/devShells.nix
+++ nix/devShells.nix
@@ -40,10 +40,41 @@
         };
 
         list = {
+          category = "Packages";
           description = "see available packages";
-          exec = lib.concatMapStrings (p: ''
-            printf '%s\n' ${lib.escapeShellArg p}
-          '') (builtins.attrNames config.packages);
+          exec =
+            ''
+              tput bold
+              echo -n 'Packages available for ' >&2
+              tput setaf 4 # blue
+              echo -n 'nix build .#' >&2
+              tput setaf 5 # magenta
+              #shellcheck disable=SC2016
+              echo -n '$PACKAGE' >&2
+              tput sgr0 # reset
+              tput bold
+              echo : >&2
+              tput sgr0 # reset
+            '' +
+            lib.concatMapStrings (p: ''
+              printf '%s\n' ${lib.escapeShellArg p}
+            '') (builtins.attrNames config.packages);
+        };
+
+        build-plugins = {
+          category = "Packages → Plugins";
+          description = "build all plugins";
+          exec = lib.pipe config.packages [
+            builtins.attrNames
+            (builtins.filter (lib.hasPrefix "cizero-plugin-"))
+            (ks: ''
+              declare -a attrs
+              for attr in ${lib.escapeShellArgs ks}; do
+                  attrs+=(".#$attr")
+              done
+              nix build --no-link --print-out-paths "''${attrs[@]}"
+            '')
+          ];
         };
       };
     };
