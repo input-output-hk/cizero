ce764d1cfde722e18fa5c87e10b64bbf1b156614
Robin Stumm <robin.stumm@iohk.io>
2023-10-13 13:24:03 +0200

    nitpick nix code style

--- nix/devShells.nix
+++ nix/devShells.nix
@@ -1,27 +1,29 @@
 {
   perSystem = { config, pkgs, ... }: {
-    devShells.default = pkgs.mkShell {
-      packages = [ config.packages.zls pkgs.just ];
-      inputsFrom = [ config.packages.cizero ];
-    };
+    devShells = {
+      default = pkgs.mkShell {
+        packages = [ config.packages.zls pkgs.just ];
+        inputsFrom = [ config.packages.cizero ];
+      };
 
-    devShells.crystal = let
-      wasi-libs = pkgs.fetchurl {
-        name = "wasm32-wasi-libs-0.0.3";
-        recursiveHash = true;
-        downloadToTemp = true;
-        url =  "https://github.com/lbguilherme/wasm-libs/releases/download/0.0.3/wasm32-wasi-libs.tar.gz";
-        hash = "sha256-GCaU5teS8fLjpq0pE98+Tyia7HiiigwFFAVqo3PpRXo=";
-        postFetch = ''
-          mkdir -p $out
-          tar xvf $downloadedFile -C $out
+      crystal = pkgs.mkShell {
+        packages = with pkgs; [ crystal crystalline llvmPackages_16.bintools wasm-tools just ];
+        shellHook = let
+          wasi-libs = pkgs.fetchurl {
+            name = "wasm32-wasi-libs-0.0.3";
+            recursiveHash = true;
+            downloadToTemp = true;
+            url =  "https://github.com/lbguilherme/wasm-libs/releases/download/0.0.3/wasm32-wasi-libs.tar.gz";
+            hash = "sha256-GCaU5teS8fLjpq0pE98+Tyia7HiiigwFFAVqo3PpRXo=";
+            postFetch = ''
+              mkdir -p $out
+              tar xvf $downloadedFile -C $out
+            '';
+          };
+        in ''
+          ln -sfT ${wasi-libs} wasi-libs
         '';
       };
-     in pkgs.mkShell {
-      packages = [ pkgs.crystal pkgs.crystalline pkgs.llvmPackages_16.bintools pkgs.wasm-tools pkgs.just ];
-      shellHook = ''
-        ln -sfT ${wasi-libs} wasi-libs
-      '';
     };
   };
 }
