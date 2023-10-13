27efb7d8799a75f41dda9d4caff09247a8635fa1
Robin Stumm <robin.stumm@iohk.io>
2023-10-13 16:56:14 +0200

    add mission-control

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     parts.url = github:hercules-ci/flake-parts;
+    mission-control.url = github:Platonic-Systems/mission-control;
+    flake-root.url = github:srid/flake-root;
     inclusive = {
       url = github:input-output-hk/nix-inclusive;
       inputs.stdlib.follows = "parts/nixpkgs-lib";
--- nix/devShells.nix
+++ nix/devShells.nix
@@ -1,13 +1,50 @@
 { inputs, ... }: {
-  perSystem = { config, pkgs, ... }: {
+  imports = with inputs; [
+    mission-control.flakeModule
+    flake-root.flakeModule
+  ];
+
+  perSystem = { config, lib, pkgs, ... }: {
     devShells = {
       default = pkgs.mkShell {
         packages = [ config.packages.zls ];
-        inputsFrom = [ config.packages.cizero ];
+        inputsFrom = [
+          config.mission-control.devShell
+          config.packages.cizero
+        ];
       };
 
       crystal = pkgs.mkShell {
         packages = with pkgs; [ crystal crystalline ];
+        inputsFrom = [ config.mission-control.devShell ];
+      };
+    };
+
+    mission-control = {
+      wrapperName = "just";
+      scripts = {
+        run = {
+          description = "run cizero without any plugins";
+          exec = ''
+            nix run .#cizero
+          '';
+        };
+
+        run-plugins = {
+          description = "run cizero with all plugins";
+          exec = ''
+            result=$(nix build --no-link --print-out-paths)
+            set -x
+            "$result"/bin/cizero "$result"/libexec/cizero/plugins/*
+          '';
+        };
+
+        list = {
+          description = "see available packages";
+          exec = lib.concatMapStrings (p: ''
+            printf '%s\n' ${lib.escapeShellArg p}
+          '') (builtins.attrNames config.packages);
+        };
       };
     };
   };
