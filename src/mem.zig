aaa035383a4f77e432e98705e23eafb6dea35a2d
Robin Stumm <serverkorken@gmail.com>
2023-12-28 12:13:03 +0100

    fix `sizeOfUnpad()` and add `paddingOf()`

--- src/mem.zig
+++ src/mem.zig
@@ -1,15 +1,32 @@
-const testing = @import("std").testing;
+const std = @import("std");
 
 /// Like `@sizeOf()` without padding.
-pub fn sizeOfUnpad(comptime T: type) usize {
-    const size_t = @bitSizeOf(T);
-    const size_u8 = @bitSizeOf(u8);
-    return size_t / size_u8 + size_t % size_u8;
+pub fn sizeOfUnpad(comptime T: type) comptime_int {
+    return std.math.divCeil(comptime_int, @bitSizeOf(T), 8) catch unreachable;
 }
 
 test sizeOfUnpad {
-    try testing.expectEqual(@as(usize, 3), sizeOfUnpad(packed struct {
-        a: u8,
-        b: u16,
-    }));
+    try std.testing.expectEqual(@as(usize, 0), sizeOfUnpad(u0));
+    try std.testing.expectEqual(@as(usize, 1), sizeOfUnpad(u8));
+    try std.testing.expectEqual(@as(usize, 2), sizeOfUnpad(u16));
+    try std.testing.expectEqual(@as(usize, 3), sizeOfUnpad(u17));
+    try std.testing.expectEqual(@as(usize, 3), sizeOfUnpad(u23));
+    try std.testing.expectEqual(@as(usize, 3), sizeOfUnpad(u24));
+    try std.testing.expectEqual(@as(usize, 4), sizeOfUnpad(u25));
+    try std.testing.expectEqual(@as(usize, 4), sizeOfUnpad(u32));
+    try std.testing.expectEqual(@as(usize, 5), sizeOfUnpad(u33));
+}
+
+pub fn paddingOf(comptime T: type) comptime_int {
+    return @sizeOf(T) - sizeOfUnpad(T);
+}
+
+test paddingOf {
+    try std.testing.expectEqual(@as(usize, 0), paddingOf(u8));
+    try std.testing.expectEqual(@as(usize, 0), paddingOf(u16));
+    try std.testing.expectEqual(@as(usize, 1), paddingOf(u17));
+    try std.testing.expectEqual(@as(usize, 1), paddingOf(u24));
+    try std.testing.expectEqual(@as(usize, 0), paddingOf(u25));
+    try std.testing.expectEqual(@as(usize, 0), paddingOf(u32));
+    try std.testing.expectEqual(@as(usize, 3), paddingOf(u33));
 }
