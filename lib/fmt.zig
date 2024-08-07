20cae0f219768a046d5be47b1e07f1966978f944
Robin Stumm <serverkorken@gmail.com>
2024-08-07 16:32:13 +0200

    fmt lib: remove unused function

--- lib/fmt.zig
+++ lib/fmt.zig
@@ -3,10 +3,6 @@ const std = @import("std");
 pub const Oneline = struct {
     str: []const u8,
 
-    pub fn init(str: []const u8) @This() {
-        return .{ .str = str };
-    }
-
     pub fn format(self: @This(), comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
         for (self.str) |char| try switch (char) {
             '\n' => writer.writeByte(' '),
