b137e51b42561324b93cd5dfcde610dde93b6fa4
Robin Stumm <serverkorken@gmail.com>
2024-08-07 16:32:33 +0200

    fmt lib: add `join()`

--- lib/fmt.zig
+++ lib/fmt.zig
@@ -15,3 +15,21 @@ pub const Oneline = struct {
 pub fn oneline(str: []const u8) Oneline {
     return .{ .str = str };
 }
+
+/// Formats strings by simply printing them separated by a separator.
+/// Useful if you don't want the `{a, b}` style for slices of strings with `{s}`.
+pub const Join = struct {
+    strs: []const []const u8,
+    sep: []const u8 = " ",
+
+    pub fn format(self: @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        for (self.strs, 0..) |str, idx| {
+            if (idx != 0) try writer.writeAll(self.sep);
+            try std.fmt.formatText(str, fmt, options, writer);
+        }
+    }
+};
+
+pub fn join(sep: []const u8, strs: []const []const u8) Join {
+    return .{ .sep = sep, .strs = strs };
+}
