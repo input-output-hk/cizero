d6937caf96f9859b8bc6a5a8f39f202f4b9ac01c
Robin Stumm <serverkorken@gmail.com>
2024-08-20 10:51:49 +0200

    posix lib: fix busy wait loop in `proxyDuplex()`

--- lib/posix.zig
+++ lib/posix.zig
@@ -65,51 +65,65 @@ pub fn proxyDuplex(
 
             /// Returns the number of bytes read from the file descriptor or null
             /// if it was not ready for reading or `options.fifo_max_size` would be exceeded.
-            fn handleReadEnd(fns: @This(), poll_fd: posix.pollfd, fifo: *std.fifo.LinearFifo(u8, .Dynamic)) !?usize {
-                if (poll_fd.revents & POLL.IN == 0) return null;
+            fn handleReadEnd(fns: @This(), src_poll_fd: *posix.pollfd, fifo: *std.fifo.LinearFifo(u8, .Dynamic), dst_poll_fd: *std.posix.pollfd) !?usize {
+                if (src_poll_fd.revents & POLL.IN == 0) return null;
 
                 var buf: [buf_size]u8 = undefined;
                 const max_read = if (fns.options.fifo_max_size) |fifo_max_size|
                     @min(fifo.readableLength() + buf.len, fifo_max_size) - fifo.readableLength()
                 else
                     buf.len;
-                if (max_read == 0) return null;
+                if (max_read == 0) {
+                    // The fifo for this file descriptor is full.
+                    // Stop polling for new data as that would result in a busy wait loop.
+                    src_poll_fd.events &= ~@as(@TypeOf(src_poll_fd.events), POLL.IN);
+                    return null;
+                }
 
-                const num_read = try posix.read(poll_fd.fd, buf[0..max_read]);
+                // There is now new data in the fifo that we want to write,
+                // so poll for the destination becoming ready for writing.
+                dst_poll_fd.events |= @as(@TypeOf(dst_poll_fd.events), POLL.OUT);
+
+                const num_read = try posix.read(src_poll_fd.fd, buf[0..max_read]);
                 try fifo.write(buf[0..num_read]);
 
                 return num_read;
             }
 
-            fn handleWriteEnd(fns: @This(), poll_fd: posix.pollfd, fifo: *std.fifo.LinearFifo(u8, .Dynamic)) !void {
-                if (poll_fd.revents & POLL.OUT == 0) return;
+            fn handleWriteEnd(fns: @This(), src_poll_fd: *posix.pollfd, fifo: *std.fifo.LinearFifo(u8, .Dynamic), dst_poll_fd: *posix.pollfd) !void {
+                if (dst_poll_fd.revents & POLL.OUT == 0) return;
 
-                const num_bytes = posix.write(poll_fd.fd, fifo.readableSlice(0)) catch |err| return switch (err) {
+                const num_written = posix.write(dst_poll_fd.fd, fifo.readableSlice(0)) catch |err| return switch (err) {
                     error.WouldBlock => {}, // retry next time
                     else => err,
                 };
-                fifo.discard(num_bytes);
+                fifo.discard(num_written);
+
+                if (num_written != 0) {
+                    // We have freed up space in the fifo
+                    // so we can poll for new data to read into it.
+                    src_poll_fd.events |= @as(@TypeOf(src_poll_fd.events), POLL.IN);
+
+                    if (fifo.readableLength() == 0) {
+                        // The fifo is empty so we don't need to poll for ready for writing
+                        // because we have nothing to write anyway.
+                        dst_poll_fd.events &= ~@as(@TypeOf(dst_poll_fd.events), POLL.OUT);
+                    }
+                }
 
                 if (fns.options.fifo_desired_size) |fifo_desired_size|
-                    if (fifo.readableLength() <= fifo_desired_size and fifo.readableLength() + num_bytes > fifo_desired_size)
+                    if (fifo.readableLength() <= fifo_desired_size and fifo.readableLength() + num_written > fifo_desired_size)
                         fifo.shrink(fifo_desired_size);
             }
         }{ .options = options };
 
-        if (try fns.handleReadEnd(poll_fds[0], &fifo_up)) |num_read|
+        if (try fns.handleReadEnd(&poll_fds[0], &fifo_up, &poll_fds[1])) |num_read|
             if (num_read == 0) return .downstream_eof;
-        if (try fns.handleReadEnd(poll_fds[1], &fifo_down)) |num_read|
+        if (try fns.handleReadEnd(&poll_fds[1], &fifo_down, &poll_fds[0])) |num_read|
             if (num_read == 0) return .upstream_eof;
 
-        try fns.handleWriteEnd(poll_fds[0], &fifo_down);
-        try fns.handleWriteEnd(poll_fds[1], &fifo_up);
-
-        inline for (.{ &fifo_up, &fifo_down }, .{ &poll_fds[1], &poll_fds[0] }) |fifo, poll_fd| {
-            if (fifo.readableLength() == 0)
-                poll_fd.events &= ~@as(@TypeOf(poll_fd.events), POLL.OUT)
-            else
-                poll_fd.events |= @as(@TypeOf(poll_fd.events), POLL.OUT);
-        }
+        try fns.handleWriteEnd(&poll_fds[0], &fifo_up, &poll_fds[1]);
+        try fns.handleWriteEnd(&poll_fds[1], &fifo_down, &poll_fds[0]);
 
         inline for (poll_fds, 0..) |poll_fd, idx| {
             if (poll_fd.revents & POLL.HUP != 0)
