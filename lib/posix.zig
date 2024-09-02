92ace9cdd8a10c507ed82ea21abc98a84b26bb0d
Robin Stumm <serverkorken@gmail.com>
2024-09-03 00:58:28 +0200

    nix-sigstop: use `recv()`/`send()` instead of `read()`/`write()` for daemon proxy socket

--- lib/posix.zig
+++ lib/posix.zig
@@ -3,8 +3,21 @@ const std = @import("std");
 
 const fmt = @import("fmt.zig");
 const mem = @import("mem.zig");
+const meta = @import("meta.zig");
 
 const posix = std.posix;
+
+pub const FileHandleType = enum {
+    fd,
+    socket,
+
+    pub fn @"type"(self: @This()) type {
+        return switch (self) {
+            .fd => posix.fd_t,
+            .socket => posix.socket_t,
+        };
+    }
+};
 
 pub const ProxyDuplexFinish = enum {
     /// The `cancel` file descriptor, if any, became ready.
@@ -20,8 +33,17 @@ pub const ProxyDuplexFinish = enum {
 
 pub fn proxyDuplex(
     allocator: std.mem.Allocator,
-    downstream: posix.fd_t,
-    upstream: posix.fd_t,
+    // These cannot be comptime fields of `options`
+    // as when the caller tries to change them from their defaults
+    // we hit https://github.com/ziglang/zig/issues/19985.
+    comptime comptime_options: struct {
+        downstream_kind: FileHandleType = .fd,
+        upstream_kind: FileHandleType = .fd,
+        /// Read/write buffer size on the stack.
+        buf_size: usize = 4 * mem.b_per_kib,
+    },
+    downstream: comptime_options.downstream_kind.type(),
+    upstream: comptime_options.upstream_kind.type(),
     /// Will return `.canceled` when this file descriptor
     /// is ready for reading, ready for writing,
     /// or, if it is the read end of a pipe,
@@ -35,13 +57,6 @@ pub fn proxyDuplex(
         fifo_max_size_behavior: enum { block, oom } = .oom,
         /// Will shrink the buffer back down to this size if possible.
         fifo_desired_size: ?usize = 512 * mem.b_per_kib,
-    },
-    // These cannot be comptime fields of `options`
-    // as when the caller tries to change them from their defaults
-    // we hit https://github.com/ziglang/zig/issues/19985.
-    comptime comptime_options: struct {
-        /// Read/write buffer size on the stack.
-        buf_size: usize = 4 * mem.b_per_kib,
     },
 ) !ProxyDuplexFinish {
     // If the `buf_size` is as large as the `fifo_max_size`
@@ -95,6 +110,7 @@ pub fn proxyDuplex(
             fn handleRead(
                 fns: @This(),
                 src_eof: *bool,
+                src_kind: FileHandleType,
                 src_poll_fd: *posix.pollfd,
                 fifo: *std.fifo.LinearFifo(u8, .Dynamic),
                 dst_poll_fd: *std.posix.pollfd,
@@ -116,7 +132,13 @@ pub fn proxyDuplex(
                     };
                 }
 
-                const num_read = try posix.read(src_poll_fd.fd, buf[0..max_read]);
+                const num_read = switch (src_kind) {
+                    .fd => posix.read(src_poll_fd.fd, buf[0..max_read]),
+                    .socket => posix.recv(src_poll_fd.fd, buf[0..max_read], 0),
+                } catch |err| switch (err) {
+                    error.WouldBlock => unreachable, // There is at least one byte available.
+                    else => |e| return e,
+                };
 
                 if (num_read != 0) {
                     try fifo.write(buf[0..num_read]);
@@ -137,13 +159,17 @@ pub fn proxyDuplex(
                 fns: @This(),
                 src_poll_fd: *posix.pollfd,
                 fifo: *std.fifo.LinearFifo(u8, .Dynamic),
+                dst_kind: FileHandleType,
                 dst_poll_fd: *posix.pollfd,
             ) !void {
                 if (dst_poll_fd.revents & POLL.OUT == 0) return;
 
-                const num_written = posix.write(dst_poll_fd.fd, fifo.readableSlice(0)) catch |err| return switch (err) {
-                    error.WouldBlock => {}, // retry next time
-                    else => err,
+                const num_written = switch (dst_kind) {
+                    .fd => posix.write(dst_poll_fd.fd, fifo.readableSlice(0)),
+                    .socket => posix.send(dst_poll_fd.fd, fifo.readableSlice(0), 0),
+                } catch |err| return switch (err) {
+                    error.WouldBlock => unreachable, // There is at least one byte available.
+                    else => |e| return e,
                 };
 
                 if (num_written != 0) {
@@ -166,11 +192,11 @@ pub fn proxyDuplex(
             }
         }{ .options = options };
 
-        try fns.handleRead(&downstream_eof, &poll_fds[0], &fifo_up, &poll_fds[1]);
-        try fns.handleRead(&upstream_eof, &poll_fds[1], &fifo_down, &poll_fds[0]);
+        try fns.handleRead(&downstream_eof, comptime_options.downstream_kind, &poll_fds[0], &fifo_up, &poll_fds[1]);
+        try fns.handleRead(&upstream_eof, comptime_options.upstream_kind, &poll_fds[1], &fifo_down, &poll_fds[0]);
 
-        try fns.handleWrite(&poll_fds[0], &fifo_up, &poll_fds[1]);
-        try fns.handleWrite(&poll_fds[1], &fifo_down, &poll_fds[0]);
+        try fns.handleWrite(&poll_fds[0], &fifo_up, comptime_options.upstream_kind, &poll_fds[1]);
+        try fns.handleWrite(&poll_fds[1], &fifo_down, comptime_options.downstream_kind, &poll_fds[0]);
 
         if (downstream_eof and fifo_up.readableLength() == 0 and
             upstream_eof and fifo_down.readableLength() == 0)
@@ -187,7 +213,7 @@ pub fn proxyDuplex(
             }
 
             if (poll_fd.revents & POLL.NVAL != 0)
-                unreachable; // always a race condition
+                unreachable; // Always a race condition.
         }
 
         if (poll_fds[2].revents & (POLL.IN | POLL.OUT) != 0)
@@ -201,78 +227,98 @@ pub fn proxyDuplex(
 /// This helps avoid using a reused FD that was closed
 /// in between reads/writes because you get the error immediately
 /// as opposed to on the next read/write.
-pub const PollingStream = struct {
-    fd: posix.fd_t,
+pub fn PollingStream(comptime kind: FileHandleType) type {
+    return struct {
+        handle: kind.type(),
 
-    pub const ReadError = posix.ReadError || posix.PollError;
-    pub const Reader = std.io.Reader(@This(), ReadError, read);
+        pub const ReadError = posix.PollError || meta.ErrorSetExcluding(switch (kind) {
+            .fd => posix.ReadError,
+            .socket => posix.RecvFromError,
+        }, &.{error.WouldBlock});
+        pub const Reader = std.io.Reader(@This(), ReadError, read);
 
-    pub const WriteError = posix.WriteError || posix.PollError;
-    pub const Writer = std.io.Writer(@This(), WriteError, write);
+        pub const WriteError = posix.PollError || meta.ErrorSetExcluding(switch (kind) {
+            .fd => posix.WriteError,
+            .socket => posix.SendError,
+        }, &.{error.WouldBlock});
+        pub const Writer = std.io.Writer(@This(), WriteError, write);
 
-    /// Returns `error.NotOpenForReading` on `POLL.HUP` and `POLL.ERR`.
-    pub fn read(self: @This(), buf: []u8) ReadError!usize {
-        const POLL = posix.POLL;
+        /// Returns `error.NotOpenForReading` on `POLL.HUP` and `POLL.ERR`.
+        pub fn read(self: @This(), buf: []u8) ReadError!usize {
+            const POLL = posix.POLL;
 
-        var poll_fds = [1]posix.pollfd{
-            .{
-                .fd = self.fd,
-                .events = POLL.IN,
-                .revents = undefined,
-            },
-        };
+            var poll_fds = [1]posix.pollfd{
+                .{
+                    .fd = self.handle,
+                    .events = POLL.IN,
+                    .revents = undefined,
+                },
+            };
 
-        std.debug.assert(try posix.poll(&poll_fds, -1) != 0);
+            std.debug.assert(try posix.poll(&poll_fds, -1) != 0);
 
-        if (poll_fds[0].revents & POLL.IN != 0)
-            return posix.read(poll_fds[0].fd, buf);
+            if (poll_fds[0].revents & POLL.IN != 0)
+                return switch (kind) {
+                    .fd => posix.read(poll_fds[0].fd, buf),
+                    .socket => posix.recv(poll_fds[0], buf, 0),
+                } catch |err| switch (err) {
+                    error.WouldBlock => unreachable, // There is at least one byte available.
+                    else => |e| return e,
+                };
 
-        if (poll_fds[0].revents & (POLL.HUP | POLL.ERR) != 0)
-            return error.NotOpenForReading;
+            if (poll_fds[0].revents & (POLL.HUP | POLL.ERR) != 0)
+                return error.NotOpenForReading;
 
-        if (poll_fds[0].revents & POLL.NVAL != 0)
-            std.debug.panic(
-                "polled invalid FD {d} in {}",
-                .{ poll_fds[0].fd, fmt.fmtSourceLocation(@src()) },
-            );
+            if (poll_fds[0].revents & POLL.NVAL != 0)
+                std.debug.panic(
+                    "polled invalid FD {d} in {}",
+                    .{ poll_fds[0].fd, fmt.fmtSourceLocation(@src()) },
+                );
 
-        unreachable;
-    }
+            unreachable;
+        }
 
-    /// Returns `error.NotOpenForWriting` on `POLL.HUP` and `POLL.ERR`.
-    pub fn write(self: @This(), buf: []const u8) WriteError!usize {
-        const POLL = posix.POLL;
+        /// Returns `error.NotOpenForWriting` on `POLL.HUP` and `POLL.ERR`.
+        pub fn write(self: @This(), buf: []const u8) WriteError!usize {
+            const POLL = posix.POLL;
 
-        var poll_fds = [1]posix.pollfd{
-            .{
-                .fd = self.fd,
-                .events = POLL.OUT,
-                .revents = undefined,
-            },
-        };
+            var poll_fds = [1]posix.pollfd{
+                .{
+                    .fd = self.handle,
+                    .events = POLL.OUT,
+                    .revents = undefined,
+                },
+            };
 
-        std.debug.assert(try posix.poll(&poll_fds, -1) != 0);
+            std.debug.assert(try posix.poll(&poll_fds, -1) != 0);
 
-        if (poll_fds[0].revents & POLL.OUT != 0)
-            return posix.write(poll_fds[0].fd, buf);
+            if (poll_fds[0].revents & POLL.OUT != 0)
+                return switch (kind) {
+                    .fd => posix.write(poll_fds[0].fd, buf),
+                    .socket => posix.send(poll_fds[0].fd, buf, 0),
+                } catch |err| switch (err) {
+                    error.WouldBlock => unreachable, // There is at least one byte available.
+                    else => |e| e,
+                };
 
-        if (poll_fds[0].revents & (POLL.HUP | POLL.ERR) != 0)
-            return error.NotOpenForWriting;
+            if (poll_fds[0].revents & (POLL.HUP | POLL.ERR) != 0)
+                return error.NotOpenForWriting;
 
-        if (poll_fds[0].revents & POLL.NVAL != 0)
-            std.debug.panic(
-                "polled closed FD {d} in {}",
-                .{ poll_fds[0].fd, fmt.fmtSourceLocation(@src()) },
-            );
+            if (poll_fds[0].revents & POLL.NVAL != 0)
+                std.debug.panic(
+                    "polled closed FD {d} in {}",
+                    .{ poll_fds[0].fd, fmt.fmtSourceLocation(@src()) },
+                );
 
-        unreachable;
-    }
+            unreachable;
+        }
 
-    pub fn reader(self: @This()) Reader {
-        return .{ .context = self };
-    }
+        pub fn reader(self: @This()) Reader {
+            return .{ .context = self };
+        }
 
-    pub fn writer(self: @This()) Writer {
-        return .{ .context = self };
-    }
-};
+        pub fn writer(self: @This()) Writer {
+            return .{ .context = self };
+        }
+    };
+}
