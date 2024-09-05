044258abb207af5726c9f50f0f6e68e2f89abda2
Robin Stumm <serverkorken@gmail.com>
2024-09-05 22:55:05 +0200

    nix-sigstop: add `meta.mainProgram` to nix package

--- nix/packages/nix-sigstop.nix
+++ nix/packages/nix-sigstop.nix
@@ -9,6 +9,8 @@
       zigDepsHash = "sha256-qlJqP2rac/xxQzOekUi9aYYCAdQj81ce7L90f1Rvzzk=";
 
       buildZigZon = "nix-sigstop/build.zig.zon";
+
+      meta.mainProgram = "nix-sigstop";
     };
   };
 }
