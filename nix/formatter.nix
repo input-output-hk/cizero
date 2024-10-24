61da520ac73d03d546995b84501fc32859e7cad5
Robin Stumm <serverkorken@gmail.com>
2024-10-24 12:47:17 +0200

    fix flake format check

--- nix/formatter.nix
+++ nix/formatter.nix
@@ -7,7 +7,10 @@
     projectRootFile = "flake.nix";
     programs = {
       alejandra.enable = true;
-      deadnix.enable = true;
+      deadnix = {
+        enable = true;
+        no-lambda-pattern-names = true;
+      };
       statix = {
         enable = true;
         disabled-lints = [
